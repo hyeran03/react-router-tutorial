a2b87c15e79b56a9b51ad483ba643a158db907d8
hyeran03 <user@email.mail>
2019-12-20 17:06:16 +0900

    라우터 부가 기능 - switch(여러가지 라우터 중 가장 먼저 매칭된 라우터 하나만 보여줌)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 // import Profile from "./Profile";
@@ -25,11 +25,22 @@ function App() {
           </li>
         </ul>
         <hr />
-
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/about" component={About} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
+        {/* 페이지를 찾지 못했을때, not found 페이지를 보여줄때 사용 */}
+        <Switch>
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/about" component={About} />
+          <Route path="/profiles" component={Profiles} />
+          <Route path="/history" component={HistorySample} />
+          {/* switch로 감싸면 위의 route 모두를 만족하지 못했으므로 다음이 실행된다 */}
+          <Route
+            render={({ location }) => (
+              <div>
+                <h2>이 페이지는 존재하지 않습니다.</h2>
+                <p>{location.pathname}</p>
+              </div>
+            )}
+          />
+        </Switch>
       </div>
     </>
   );
