fe7c2a462522e8975c240b370ef9731f57bed38b
hyeran03 <user@email.mail>
2019-12-20 15:25:43 +0900

    서브라우트

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+// import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 function App() {
   return (
@@ -15,11 +16,15 @@ function App() {
           <li>
             <Link to="/about">소개</Link>
           </li>
+          <li>
+            <Link to="/profiles">프로필 목록</Link>
+          </li>
         </ul>
         <hr />
+
         <Route path="/" component={Home} exact={true} />
         <Route path="/about" component={About} />
-        <Route path="/profiles/:username" component={Profile} />
+        <Route path="/profiles" component={Profiles} />
       </div>
     </>
   );
